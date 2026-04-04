0ac0d2205c3d2e828bdd53511cd9907a1b9d3cbc
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-04-04 10:33:57 +0530

    DAY 1

---
+++ PrintTheSpiralMatrix.java
@@ -0,0 +1,62 @@
+/*
+Given an M * N matrix, print the elements in a clockwise spiral manner.
+
+Return an array with the elements in the order of their appearance when printed in a spiral manner.
+
+Example 1
+Input: matrix = [[1, 2, 3], [4 ,5 ,6], [7, 8, 9]]
+Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+Explanation:
+The elements in the spiral order are 1, 2, 3 -> 6, 9 -> 8, 7 -> 4, 5
+
+Example 2
+Input: matrix = [[1, 2, 3, 4], [5, 6, 7, 8]]
+Output: [1, 2, 3, 4, 8, 7, 6, 5]
+Explanation:
+The elements in the spiral order are 1, 2, 3, 4 -> 8, 7, 6, 5
+
+Example 3
+Input: matrix = [[1, 2], [3, 4], [5, 6], [7, 8]]
+Output: [1, 2, 4, 6, 8, 7, 5, 3]
+*/
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int left = 0;
+        int bottom = n - 1;
+        int right = m - 1;
+
+        while(top <= bottom && left <= right){
+
+            for(int i = left; i <= right; i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i = top; i <= bottom; i++){
+                ans.add(matrix[i][right]);
+            }
+
+            right--;
+
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
