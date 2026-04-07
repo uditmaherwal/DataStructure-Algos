37defc89bbeeb3ca35f65013eae5a325e08738b3
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-04-07 23:13:02 +0530

    DAY 3

---
+++ Rotate_matrix_by_90_Degress.java
@@ -0,0 +1,45 @@
+/* 
+Rotate matrix by 90 degrees
+
+Given an N * N 2D integer matrix, rotate the matrix by 90 degrees clockwise.
+
+The rotation must be done in place, meaning the input 2D matrix must be modified directly.
+Example 1
+Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+Output: matrix = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+Example 2
+Input: matrix = [[0, 1, 1, 2], [2, 0, 3, 1], [4, 5, 0, 5], [5, 6, 7, 0]]
+
+Output: matrix = [[5, 4, 2, 0], [6, 5, 0, 1], [7, 0, 3, 1], [0, 5, 1, 2]]
+
+Example 3
+Input: matrix = [[1, 1, 2], [5, 3, 1], [5, 3, 5]]
+Output:
+[[5, 5, 1], [3, 3, 1], [5, 1, 2]]
+
+*/
+
+class Solution {
+    public void rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+}
